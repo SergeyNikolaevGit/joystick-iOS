1613eb4bd16900a260cd4dce266da5d52ae63962
Sergey Nikolaev <sergey@sergeys-macbook-pro.local>
2018-08-22 11:42:51 +0300

    Initial Commit

---
+++ Joystic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Joystic
+//
+//  Created by Sergey Nikolaev on 22/08/2018.
+//  Copyright © 2018 Sergey Nikolaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
